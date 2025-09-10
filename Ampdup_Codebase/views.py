ea2868ade965d63a250fed87be577dd33d51f8f6
erix-senpai <erichsu940927@gmail.com>
2025-09-10 14:03:28 +1000

    Fixed the site linking via changing the hardcode href to the @main.bp function via {{ url_for('main.__filename__') }}. Additionally, filename for "Event Details" is changed to "Event_Details", html file is not meant to have spacings.

--- Ampdup_Codebase/views.py
+++ Ampdup_Codebase/views.py
@@ -1,7 +1,19 @@
 from flask import Blueprint, render_template
 
-mainbp = Blueprint('main', __name__)
+mainbp = Blueprint('main', __name__ , template_folder='../templates')
 
 @mainbp.route('/')
 def index():
     return render_template('index.html')
+
+@mainbp.route('/BookingHistory')
+def BookingHistory():
+    return render_template('BookingHistory.html')
+
+@mainbp.route('/CreateEvent')
+def CreateEvent():
+    return render_template('CreateEvent.html')
+
+@mainbp.route('/Event_Details')
+def Event_Details():
+    return render_template('Event_Details.html')
