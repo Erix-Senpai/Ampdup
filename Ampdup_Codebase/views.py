2fa8368e940233fd9257747911f02bc44e3eea6f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-09-15 15:54:21 +1000

    test

--- Ampdup_Codebase/views.py
+++ Ampdup_Codebase/views.py
@@ -5,3 +5,15 @@ mainbp = Blueprint('main', __name__)
 @mainbp.route('/')
 def index():
     return render_template('index.html')
+
+@mainbp.route('/eventdetails')
+def eventdetails():
+    return render_template('Event Details.html')
+
+@mainbp.route('/bookinghistory')
+def bookinghistory():
+    return render_template('BookingHistory.html')
+
+@mainbp.route('/createevent')
+def createevent():
+    return render_template('CreateEvent.html')
