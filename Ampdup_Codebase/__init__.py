af8bdc0f8cc39cc9a85ce9b660336b71a5512121
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-09-22 22:46:39 +1000

    added login and registration forms

--- Ampdup_Codebase/__init__.py
+++ Ampdup_Codebase/__init__.py
@@ -4,16 +4,10 @@ from flask_bootstrap import Bootstrap5
 def create_app():
     app = Flask(__name__)
     
-    app.secret_key = 'somesecretkey'
-    # set the app configuration data 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sitedata.sqlite'
-    # initialise db with flask app
-    #db.init_app(app)
-
     Bootstrap5(app)
-    
 
-
+    # A secret key for the session object
+    app.secret_key = 'somerandomvalue'
 
     #add Blueprints
     from . import views
@@ -22,7 +16,7 @@ def create_app():
     app.register_blueprint(create_event.create_event_bp)
     from . import destinations
     app.register_blueprint(destinations.destbp)
-    from .auth import auth_bp
-    app.register_blueprint(auth_bp)
+    from . import auth
+    app.register_blueprint(auth.auth_bp)
 
     return app
